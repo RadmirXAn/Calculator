9e4a0e812022bb1f781473d6736097b5fea2c4fb
RadMirXAn <rrradmirrr@mail.ru>
2020-11-09 17:47:38 +0500

    xcode
    Калькулятор на Xcode

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var operand = ""
+    var numberOne = ""
+    var numberTwo = ""
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBAction func inputNumber(_ sender: UIButton) {
+        if operand.isEmpty{
+            numberOne = numberOne + (sender.titleLabel?.text)!
+            resultLabel.text = numberOne
+        }else{
+            numberTwo = numberTwo + (sender.titleLabel?.text)!
+            resultLabel.text = numberTwo
+        }
     }
-
-
+    
+    @IBAction func clearAction(_ sender: UIButton) {
+        operand = ""
+        numberOne = ""
+        numberTwo = ""
+        resultLabel.text = "0"
+        
+    }
+    
+    @IBAction func inputOpearnd(_ sender: UIButton) {
+        operand = sender.titleLabel?.text as! String
+    }
+    
+    @IBAction func resultLabel(_ sender: UIButton) {
+        var result = 0.0
+        switch  operand {
+        case "/":
+            result = Double(numberOne)! / Double(numberTwo)!
+        case "*":
+            result = Double(numberOne)! * Double(numberTwo)!
+        case "-":
+            result = Double(numberOne)! - Double(numberTwo)!
+        case "+":
+            result = Double(numberOne)! + Double(numberTwo)!
+        default:
+            break
+        }
+        if result.truncatingRemainder(dividingBy: 1.0) == 00{
+            resultLabel.text = String(Int(result))
+        }else{
+            resultLabel.text = String(result)
+        }
+    }
+    
+    @IBAction func stepperAction(_ sender: UIStepper) {
+        resultLabel.font = resultLabel.font.withSize(CGFloat(sender.value+48))
+    }
+    
 }
-
